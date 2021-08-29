e35b1ae712c0f90e9770fe301cd057675d860f37
Shanna <shannameow@gmail.com>
2021-08-29 18:40:42 -0400

    cleaning up code

--- frontend/src/components/categories/pages/architecture.js
+++ frontend/src/components/categories/pages/architecture.js
@@ -1,11 +1,8 @@
-import { useLocation } from "react-router";
 import ArcHeader from "../../headernavbar/catHeaders/arcHeader";
 import CategorySpace from "../../body/space/categorySpace";
 import ConnectFooter from "../../footer/connectFooter";
 
 export default function Architecture(props) {
-  const location = useLocation();
-  console.log(location);
   const user = props.user;
 
   return (
